bed6774dab10dfd5707f78167449a63de2447552
Nadia Ridko <nadia.ridko@gmail.com>
2018-02-09 12:43:20 +0100

    change jquery_methods

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
 $(document).ready(function() {
 //1
-  /*
-$("#click").click(function() {
-    $("#book").hide("slow", function() {
-      alert("click");
-    })
-  })
-// 2
-  $("button").click(function() {
-    $("p").hide("slow");
-  })
-// 3
-  $("#hide").click(function() {
-    $("span:last-child").hide("fast", function() {
-      $(this).prev().hide("fast", arguments.callee);  
-    })
-  })
-  $("#show").click(function() {
-    $("span").show(2000);
-  })
-// 4
-  for ( var i = 0; i < 5; i++ ) {
-    $( "<div>" ).appendTo( document.body );
-  }
-  $( "div" ).click(function() {
-    $( this ).hide( 2000, function() {
-      $( this ).remove();
-    });
-  });
-// 5
-  $(".text").fadeIn(4000);
 
-  // 6
-
-  $("a").each(function() {
-    var links = $(this).attr("href");
-    $("#list").append("<li>" + links + "<li>");
-  });
-  */
-  //7
   var animaTime = 2000;
   var modal = $("#modalDiv");
 
